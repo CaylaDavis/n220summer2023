16cb05fafcc7b26a3e40abe6ac3b2f0fa2be5830
CaylaDavis <137857442+CaylaDavis@users.noreply.github.com>
2023-06-30 14:40:57 -0400

    Shape composition

---
+++ js/app.js
@@ -0,0 +1,12 @@
+function setup() {
+    createCanvas (800,600);
+    var royal = ("#0324fc");
+
+    background(royal);
+    fill(245, 66, 135);
+    stroke (132,252,3);
+    strokeWeight(4)
+    triangle(200, 300, 400,400, 600, 300);
+    rect(200,300,400,300);
+
+}
