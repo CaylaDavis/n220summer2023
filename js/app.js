24d6d09293a8a47f61ba296a26db0f6bee80b172
CaylaDavis <137857442+CaylaDavis@users.noreply.github.com>
2023-06-30 18:11:39 -0400

    Spaced code for readability

--- js/app.js
+++ js/app.js
@@ -8,14 +8,19 @@ function setup() {
     stroke (132,252,3);
     strokeWeight(4);
     rect(startX,300,400,300);
+   
     fill(250, 138, 52);
     triangle(startX, 300, 400,200, 600, 300);
+   
     fill(royal);
     rect(450,400,100,100);
+    
     fill(151, 52, 250);
     rect(300,400,100,200);
+    
     line(450,450,550,450);
     line(500,400,500,500);
+    
     fill(245, 230, 66)
     noStroke()
     circle(380,500,5)
