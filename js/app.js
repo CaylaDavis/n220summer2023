9beebbccd750656ad3377d9c7ff0f06aba805916
CaylaDavis <137857442+CaylaDavis@users.noreply.github.com>
2023-06-30 22:23:04 -0400

    comments
    Info on parameters

--- js/app.js
+++ js/app.js
@@ -16,14 +16,17 @@ function setup() {
         //alter the width of the outline to be more noticeable 
     rect(startX,300,400,300);
         //create rectangle (base of house) and use variable named startX for x coordinate 
+        //(x,y,width, height)
    
     fill(250, 138, 52);
     triangle(startX, 300, 400,200, 600, 300);
         //create triangle filled with orange (roof) and use variable named startX for x coordinate 
+        //(x1,y1,x2,y2,x3,y3)
    
     fill(royal);
     square(450,400,100)
        //create square filled with exact blue of background using royal variable (window)
+       //(x,y, length of sides)
     
     fill(151, 52, 250);
     rect(300,400,100,200);
@@ -32,11 +35,12 @@ function setup() {
     line(450,450,550,450);
     line(500,400,500,500);
      // create perpendicular lines with 90 degree angles within the square (window)
-    
+    //(x1,y1,x2,y2)
     fill(245, 230, 66)
     noStroke()
     circle(380,500,5)
       //Create small circle (doorknob)
+      //(x,y,diamter)
     circle(800,20,150)
       //Create circle with paremeters that will extend over the right corner of canvas (sun)
 
