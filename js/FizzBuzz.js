7831e2cdf2a3a1619b24d62fbb4c05497c333cfe
CaylaDavis <137857442+CaylaDavis@users.noreply.github.com>
2023-07-07 18:18:04 -0400

    code for fizz buzz

--- js/FizzBuzz.js
+++ js/FizzBuzz.js
@@ -1,14 +1,20 @@
 function setup() {
-    createCanvas (400,300);
+    createCanvas (800,600);
     //canvas size 
 }
 function draw() {
     background(252, 250, 250);
-    fill(66, 155, 245);
+    
     
     //For loop syntax
-    for (var i= 0; i < 25; i++) {
-        circle (i*10,100,20)
+    for (var i= 0; i +=25; i++) {
+        fill(5,5,5);
+        circle (i*30,300,15);
+
+        if (i%3==0){
+            fill(109, 50, 168);
+            circle(i*30,300,15);
+         }
 
     }
 }
