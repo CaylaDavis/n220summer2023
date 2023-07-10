8fbb9aad9f76750917045a7288e9bb01cf09e6d2
CaylaDavis <137857442+CaylaDavis@users.noreply.github.com>
2023-07-10 06:41:00 -0400

    added comments

--- js/circle.js
+++ js/circle.js
@@ -4,11 +4,14 @@ function setup() {
 }
 
 function draw() {
-    background(211, 214, 219);
-    noFill();
+    background(211, 214, 219)
+    //set background color to light grey
 
 // For loop syntax 
     for (var i = 0; i < 30; i++){
+        noFill();
         circle(400,500,i*15)
     }
+    //create 30 circle with no fill. 
+    //The (i *15) alters the diameter of the circles so that you can see the larger circles encompass the smaller circles 
 }
