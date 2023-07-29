72a28c92c2b1fd32e4122258e6df18d31212bcfd
CaylaDavis <137857442+CaylaDavis@users.noreply.github.com>
2023-07-29 09:58:18 -0400

    index update

---
+++ js/info.js
@@ -0,0 +1,14 @@
+let stringsDiv = document.getElementById("strings")
+
+home = [ "Welcome to N220"]
+about = [ "There are weekly videos to teach the concepts related to interactive multimedia applications "]
+assignments = ["The assignments are excercies based on the week's video"]
+
+
+function showInfo1 = 
+
+
+function showInfo2 = 
+ 
+
+function showInfo3 = 
