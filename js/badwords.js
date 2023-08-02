98d4db7ee8c8544d3b099a3239ea1f30c9201c60
CaylaDavis <137857442+CaylaDavis@users.noreply.github.com>
2023-08-02 09:27:14 -0400

    Trying to get count = 0 to work

--- js/badwords.js
+++ js/badwords.js
@@ -2,30 +2,31 @@ let sentence = document.getElementById("userSentence")
 let answerDiv = document.getElementById("answer")
 
 badwords = ["clear", "water", "tires"]
-
-
+count = 0
 function wordCheck() {
     let myArray = sentence.value;
     let splitArray = myArray.split(" ");
     let lookingFor = badwords
-    count = 0 
+    let count = 0;
     for (let i = 0; i < lookingFor.length; i++) {
         for(let n = 0; n < splitArray.length; n++) {
+            count = 0
             if(lookingFor[i] == splitArray[n]) {
-                count ++;
+                count++
                 console.log(count)
-                if(count > 0) {
+                if( count > 0 ) {
                     answerDiv.innerHTML = " There are " + count + " bad words in this statement" 
-                    }
-                    else{
+                }
+                else{
                     answerDiv.innerHTML = "There are no bad words in this statement"
-                    }
+                }
+        
+
+                }
     sentence.value = " "
-                    
-                
-            }   
+            }                
+            
+              
         }
-
-    }
+        
 }
-
