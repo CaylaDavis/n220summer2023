5c9c3abc64b2fb22f9d5046d265009d18e651fa4
CaylaDavis <137857442+CaylaDavis@users.noreply.github.com>
2023-08-02 09:31:51 -0400

    fixed again

--- js/badwordsarray.js
+++ js/badwordsarray.js
@@ -10,6 +10,7 @@ function wordCheck() {
     let count = 0;
     for (let i = 0; i < lookingFor.length; i++) {
         for(let n = 0; n < splitArray.length; n++) {
+            count = 0
             if(lookingFor[i] == splitArray[n]) {
                 count++
                 console.log(count)
@@ -19,12 +20,12 @@ function wordCheck() {
                 else{
                     answerDiv.innerHTML = "There are no bad words in this statement"
                 }
-
-            }
-sentence.value = " "
-        }                
-            
-              
-     }
         
-}
+
+                }
+    sentence.value = " "
+            }                
+        }    
+              
+     } 
+      
