4a8cef9d40542cff4b37a891f08e1d76d2afb70d
CaylaDavis <137857442+CaylaDavis@users.noreply.github.com>
2023-08-02 14:52:28 -0400

    Update divs.js

--- js/divs.js
+++ js/divs.js
@@ -8,16 +8,24 @@ let objects = [
  { color: "#ff0000", height: 300, width: 100 },
 
 ];
+//array given from assignment with value pairs for color,height,and width 
 
 
 function showMe(){
     for(var i=0; i<objects.length; i++){
+    //for length of objects array
         let dvSquare = document.createElement(("div"))
+        //Create HTML element with id "div"
         dvSquare.style.backgroundColor = objects[i].color
+        //fill div color with color in objects array according to which [i] the loop is on
         dvSquare.style.height = objects[i].height + "px";
+        //adjust div height with height in objects height value
         dvSquare.style.width = objects[i].width + "px";
+        //adjust div width with with in obhect width value
         document.body.appendChild(dvSquare);
+        //add element to the body 
         }
     }
 
 showMe();
+//invoke function 
