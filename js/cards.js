26279a1338e3980e3612fa3d1ac37d0760f6382e
CaylaDavis <137857442+CaylaDavis@users.noreply.github.com>
2023-08-05 04:28:46 -0400

    comments

--- js/cards.js
+++ js/cards.js
@@ -1,9 +1,11 @@
 let question1 = document.getElementById("question1") 
+//reference HTML element 
 
 let question2 = document.getElementById("question2")
-
+//reference HTML element
 
 let question3 = document.getElementById("question3")
+//reference HTML element 
 
 
 
@@ -11,6 +13,9 @@ let question3 = document.getElementById("question3")
 
 function giveAnswer(event) {
     let answer = event.target.getAttribute("data-answer");
+    //store customized attribute for answer in variable
     event.target.innerHTML = answer ;
+    //display answer on page 
+    //event.target is used because the action of clicking invoking the function is attached to the div - div was listening for  event  
 
 }
--- js/mixer.js
+++ js/mixer.js
@@ -1,81 +1,100 @@
 let originalDiv = document.getElementById("original")
+//reference HTML element 
     r = 0 ;
+    //r stands for red
     g = 0 ;
+    //g stands for green 
     b = 0 ;
+    //b stands for blue 
     rgbColor = "rgb(" + r + "," + g + "," + b + ")"
+    //store original color (rgb(0,0,0)) in a variable
+    //black
 
 
 function styleDiv() {
+//function to style orignal square
 originalDiv.style.width =  "400px" ;
+//width of div
 originalDiv.style.height = "200px ";
+//height of div 
 originalDiv.style.backgroundColor = rgbColor
+//change color of div to variable that stores black color above
 }
 
+
 styleDiv();
+//invoke function  
 
 
 
 function red1 (){
     rgbColor = "rgb(" +  (r += 1) +  "," + g + "," + b + ")"
+    //add 1 to value for red variable and assign new number as value  
     originalDiv.style.backgroundColor = rgbColor
+    //change background color of div to new rgb value after altering red 
     console.log(rgbColor)
-    originalDiv.style.backgroundColor = rgbColor
 }
 
 function red5 (){
     rgbColor = "rgb(" +  (r += 5) +  "," + g + "," + b + ")"
+    //add 5 to value for red variable and assign new number as value 
     originalDiv.style.backgroundColor = rgbColor
+    //change  background Color of div to new rgb value after altering red 
     console.log(rgbColor)
-    originalDiv.style.backgroundColor = rgbColor
+   
 }
 
 function red10 (){
     rgbColor = "rgb(" +  (r += 10) +  "," + g + "," + b + ")"
+    ///add 10 to value for red variable and assign new number as value 
     originalDiv.style.backgroundColor = rgbColor
+    ////change  background Color of div to new rgb value after altering red 
     console.log(rgbColor)
-    originalDiv.style.backgroundColor = rgbColor
+
 }
+
+//These same directions can be used in below function. Only change is what rgb color is being altered 
 
 function green1 (){
     rgbColor = "rgb(" +  r  +  "," + ( g += 1)  + "," + b + ")"
     originalDiv.style.backgroundColor = rgbColor
     console.log(rgbColor)
-    originalDiv.style.backgroundColor = rgbColor
+    
 }
 
 function green5 (){
     rgbColor = "rgb(" +  r  +  "," + ( g += 5)  + "," + b + ")"
     originalDiv.style.backgroundColor = rgbColor
     console.log(rgbColor)
-    originalDiv.style.backgroundColor = rgbColor
+    
 }
 
 function green10 (){
     rgbColor = "rgb(" +  r  +  "," + ( g += 10)  + "," + b + ")"
     originalDiv.style.backgroundColor = rgbColor
     console.log(rgbColor)
-    originalDiv.style.backgroundColor = rgbColor
+   
 }
 
 function blue1 (){
     rgbColor = "rgb(" +  r  +  "," +  g   + "," + (b += 1) + ")"
     originalDiv.style.backgroundColor = rgbColor
     console.log(rgbColor)
-    originalDiv.style.backgroundColor = rgbColor
+   
 }
 
 function blue5 (){
     rgbColor = "rgb(" +  r  +  "," +  g   + "," + (b += 5) + ")"
     originalDiv.style.backgroundColor = rgbColor
     console.log(rgbColor)
-    originalDiv.style.backgroundColor = rgbColor
+    
 }
 
 function blue10 (){
     rgbColor = "rgb(" +  r  +  "," +  g   + "," + (b += 10) + ")"
     originalDiv.style.backgroundColor = rgbColor
     console.log(rgbColor)
-    originalDiv.style.backgroundColor = rgbColor
+    
 }
 
 
